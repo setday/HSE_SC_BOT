20a20767f037716de6bf0489668fea5a3784178a
Alexander Serkov <32331578+setday@users.noreply.github.com>
2024-07-07 23:05:11 +0300

    Remove some files

--- keys.py
+++
@@ -1,8 +0,0 @@
-BOT_API_TOKEN = "7304998058:AAGHS8iGBgNGcAOpo0zmw3lHlnbQdkOVS08"
-BACK_CHAT_ID = -4142296528
-
-def get_bot_key():
-    return BOT_API_TOKEN
-
-def get_back_id():
-    return BACK_CHAT_ID
